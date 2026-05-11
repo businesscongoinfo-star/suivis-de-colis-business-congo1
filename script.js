55a190bbae4dba897b1f76e86335bdbd98c5e572
businesscongoinfo-star <businesscongo.info@gmail.com>
2026-05-11 01:40:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbwXNsMRakqZgWl5bGIbd-i4rTqIo43qAWrOAbaw5SGLvI0G-IjcpdJeFmXUSTK2TzJL/exec";
 
-async function rechercherColis() {
+function rechercherColis() {
 
     const numero = document.getElementById("trackingNumber").value;
 
     if (!numero) {
-        alert("Entrez un numéro de suivi");
+
+        alert("Veuillez entrer un numéro de suivi.");
+
         return;
     }
 
-    try {
-
-        const response = await fetch(`${API_URL}?numero=${numero}`);
-
-        const data = await response.json();
-
-        console.log(data);
-
-        alert("Recherche effectuée");
-
-    } catch (error) {
-
-        console.error(error);
-
-        alert("Erreur lors de la recherche");
-
-    }
+    window.location.href = `${API_URL}?numero=${numero}`;
 }
