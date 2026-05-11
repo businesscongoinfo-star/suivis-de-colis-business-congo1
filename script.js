52f0f6cb4d3aad31f284ad140d1ec1656ab665bf
businesscongoinfo-star <businesscongo.info@gmail.com>
2026-05-11 01:27:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function ouvrirSite(){
 
 window.location.href =
-"https://script.google.com/macros/s/AKfycbwXNsMRakqZgWl5bGIbd-i4rTqIo43qAWrOAbaw5SGLvI0G-IjcpdJeFmXUSTK2TzJL/exec";
-
+https://script.google.com/macros/s/AKfycbwXNsMRakqZgWl5bGIbd-i4rTqIo43qAWrOAbaw5SGLvI0G-IjcpdJeFmXUSTK2TzJL/exec
 }
